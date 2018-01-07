104c8c961e25a494ddb2e2c1de0d37a238265b26
Patrick Drechsler <patrick@pdrechsler.de>
2018-01-07 23:29:45 +0100

    implemented exercise 3

--- chapter03/Exercise3.cs
+++ chapter03/Exercise3.cs
@@ -1,50 +1,60 @@
-using System;
 using Xunit;
 using FluentAssertions;
 using Functional;
 using static Functional.F;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
 using System.ComponentModel.DataAnnotations;
 
 namespace chapter03
 {
+    /*
+        Write a type Email that wraps an underlying string, enforcing that it's in a valid format. 
+        Ensure that you include the following:
+            - A smart constructor
+            - Implicit conversion to string, so that it can easily be used with the typical API for sending emails
+    */
     public class Exercise3
     {
-        /*
-            Write a type Email that wraps an underlying string, enforcing that it's in a valid format. Ensure that you include the following:
-                A smart constructor
-                Implicit conversion to string, so that it can easily be used with the typical API for sending emails
-         */
-
         public struct Email
         {
             private string Value { get; }
 
-            private Email(string value)
-            {
-                if (!IsValid(value))
-                {
-                    throw new ArgumentException("${value} is not a valid email address");
-                }
+            private Email(string value) => Value = value;
 
-                Value = value;
-            }
+            // Smart ctor
+            public static Option<Email> Create(string s)
+                => IsValid(s)
+                    ? Some(new Email(s))
+                    : None;
 
+            // Use the built-in email validation (instead of some Regex): 
+            //      EmailAddressAttribute().IsValid(...)
+            // Yes, this also works with .NET Core: Just include the following:
+            //      using System.ComponentModel.DataAnnotations;
             private static bool IsValid(string value)
-            {
-                // Use the built-in email validation (instead of some Regex): 
-                //      EmailAddressAttribute().IsValid(...)
-                // Yes, this also works with .NET Core: Just include the following:
-                //      using System.ComponentModel.DataAnnotations;
-                return new EmailAddressAttribute().IsValid(value);
-            }
+                => new EmailAddressAttribute().IsValid(value);
         }
 
-        [Fact]
-        public void Email_cannot_be_created_with_an_invalid_string()
+        [Theory]
+        [InlineData("invalid")]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("a.b")]
+        public void Email_cannot_be_created_with_an_invalid_string(string input)
         {
+            Email.Create(input)
+                .IsSome()
+                .Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("foo@bar.com")]
+        [InlineData("a@b.c")]
+        [InlineData("a@b")]
+        public void Valid_input_creates_Email(string input)
+        {
+            Email.Create(input)
+                .IsSome()
+                .Should().BeTrue();
         }
     }
 }
