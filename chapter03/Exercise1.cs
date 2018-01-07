65549b5273f5bfaf8952f5d6b0d289be22c0089c
Patrick Drechsler <patrick@pdrechsler.de>
2018-01-07 18:48:34 +0100

    exercise 1: failing tests

--- chapter03/Exercise1.cs
+++ chapter03/Exercise1.cs
@@ -1,15 +1,49 @@
 using System;
 using Xunit;
 using FluentAssertions;
+using Functional;
+using static Functional.F;
 
 namespace chapter03
 {
     public class Exercise1
     {
-        [Fact]
-        public void ParsingWeekdaysTest()
+        public enum Weekday
         {
-            // TODO
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+
+        }
+
+        public static Option<Weekday> ParseWeekday(string input)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        [Fact]
+        public void Parsing_valid_Weekdays_returns_Some()
+        {
+            ParseWeekday("Monday").Should().Be(Some<Weekday>(Weekday.Monday));
+            ParseWeekday("Tuesday").Should().Be(Some<Weekday>(Weekday.Tuesday));
+            ParseWeekday("Wednesday").Should().Be(Some<Weekday>(Weekday.Wednesday));
+            ParseWeekday("Thursday").Should().Be(Some<Weekday>(Weekday.Thursday));
+            ParseWeekday("Friday").Should().Be(Some<Weekday>(Weekday.Friday));
+            ParseWeekday("Saturday").Should().Be(Some<Weekday>(Weekday.Saturday));
+            ParseWeekday("Sunday").Should().Be(Some<Weekday>(Weekday.Sunday));
+        }
+        
+        [Fact]
+        public void Parsing_invalid_Weekdays_returns_None()
+        {
+            ParseWeekday(null).Should().Be(None);
+            ParseWeekday("").Should().Be(None);
+            ParseWeekday("Invalid").Should().Be(None);
+            ParseWeekday("WednesdayX").Should().Be(None);
         }
     }
 }
