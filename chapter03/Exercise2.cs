7ff6f400a69c25e8ee8db5ef6a6055be66315c23
Patrick Drechsler <patrick@pdrechsler.de>
2018-01-07 21:09:00 +0100

    exercise 2

--- chapter03/Exercise2.cs
+++ chapter03/Exercise2.cs
@@ -3,9 +3,27 @@ using Xunit;
 using FluentAssertions;
 using Functional;
 using static Functional.F;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace chapter03
 {
+    public static class FunctionalExtensions
+    {
+        public static Option<T> Lookup<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    return Some(item);
+                }
+            }
+            return None;
+        }
+    }
+
     // Write a Lookup function that will take an IEnumerable and a predicate, and
     // return the first element in the IEnumerable that matches the 
     // predicate, or None if no matching element is found. 
@@ -17,16 +35,25 @@ namespace chapter03
         [Fact]
         public void Empty_list_returns_None()
         {
+            new List<int>()
+                .Lookup(isOdd)
+                .IsSome().Should().BeFalse();
         }
 
         [Fact]
         public void List_with_no_odd_numbers_returns_None()
         {
+            new List<int>{ 0, 2, 4 }
+                .Lookup(isOdd)
+                .IsSome().Should().BeFalse();
         }
 
         [Fact]
         public void List_with_odd_number_included_returns_first_occurrence_of_odd_number()
         {
+            new List<int>{ 0, 1, 4 }
+                .Lookup(isOdd)
+                .IsSome().Should().BeTrue();
         }
     }
 }
