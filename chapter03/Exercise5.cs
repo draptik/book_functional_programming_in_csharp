c5f4682c070c22e885af191084d39ea55db672cd
Patrick Drechsler <patrick@pdrechsler.de>
2018-01-07 23:45:57 +0100

    no ConfigurationManager in dotnet core :-(

---
+++ chapter03/Exercise5.cs
@@ -0,0 +1,42 @@
+using Xunit;
+using FluentAssertions;
+using Functional;
+using static Functional.F;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace chapter03
+{
+    /*
+        Write implementations for the methods in the following AppConfig class. (For both methods, a reasonable one-line method body is possible. Assume the settings are of type string, numeric, or date.) Can this implementation help you to test code that relies on settings in a .config file?
+
+        "ConfigurationManager" does not exist in .NET Core. :-(
+     */
+
+    // public class AppConfig
+    // {
+    //     NameValueCollection source;
+
+    //     public AppConfig() : this(ConfigurationManager.AppSettings) { }
+
+    //     public AppConfig(NameValueCollection source)
+    //     {
+    //         this.source = source;
+    //     }
+
+    //     public Option<T> Get<T>(string name)
+    //     {
+    //         // your implementation here...
+    //     }
+
+    //     public T Get<T>(string name, T defaultValue)
+    //     {
+    //         // your implementation here...
+    //     }
+    // }   
+
+    public class Exercise5
+    {
+
+    }
+}
