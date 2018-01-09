17064ad97688f1ba34f5514db94779cbef73d456
Patrick Drechsler <patrick.drechsler@redheads.de>
2018-01-09 13:37:39 +0100

    added chapter 7 template

---
+++ chapter07/src/Exercise4.cs
@@ -0,0 +1,13 @@
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Chapter07
+{
+    public class Exercise4
+    {
+    }
+}
