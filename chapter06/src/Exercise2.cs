523d4ed88179a0fd6c5d1c5b39f11877fe854ce8
Patrick Drechsler <patrick.drechsler@redheads.de>
2018-01-09 12:33:57 +0100

    test for binding either with option

--- chapter06/src/Exercise2.cs
+++ chapter06/src/Exercise2.cs
@@ -19,6 +19,32 @@ namespace Chapter06
     // yielding an `Option`.
     public class Exercise2
     {
+        [Fact]
+        public void Binding_Either_with_Option()
+        {
+            // initial version: both functions return Option
+            // Func<string, Option<string>> f1Option = s => Some(s);
+            // Func<string, Option<string>> f2 = s => s;
+            // Func<string, Option<string>> bound1 = s => f1Option(s).Bind(f2);
 
+            // first function returns Either
+            Func<string, Either<string, string>> f1Either = s => Right(s);
+            Func<string, Option<string>> f2 = s => s;
+            Func<string, Option<string>> bound2 = s => f1Either(s).Bind(f2);
+            bound2("foo").ToString().Should().Be("Some(foo)");
+        }
+    }
+
+    public static class Ext
+    {
+        public static Option<RR> Bind<L, R, RR>(this Either<L, R> either, Func<R, Option<RR>> func)
+            => either.Match(
+                Left: _ => None,
+                Right: r => func(r));
+
+        public static Option<RR> Bind<L, R, RR>(this Option<R> option, Func<R, Either<L, RR>> func)
+            => option.Match(
+                None: () => None,
+                Some: v => func(v).ToOption());
     }
 }
