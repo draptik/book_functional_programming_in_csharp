e93fa8f9d7ba896b8a712d4e2c05e2842d64c5e5
Patrick Drechsler <patrick.drechsler@redheads.de>
2018-01-10 08:39:32 +0100

    ApplyR

--- chapter07/src/Exercise1.cs
+++ chapter07/src/Exercise1.cs
@@ -29,5 +29,21 @@ namespace Chapter07
         [InlineData(-5, 3, -2)]
         public void Remainder_is_calculated_correctly(int dividend, int divisor, int expected) 
             => Remainder(dividend, divisor).Should().Be(expected);
+
+        [Fact]
+        public void ApplyR_returns_rightmost_parameter()
+        {
+            // TODO: Revisit this later...
+            Func<int, string, bool> fun = (i, s) => true;
+            fun.ApplyR("someString").Should().BeOfType(typeof(Func<int, bool>));
+        }
+    }
+
+    public static class Extensions
+    {
+        // (((T1, T2) -> R), T2) -> T1 -> R
+        // (T1 -> T2 -> R) -> T2 -> T1 -> R (curried) 
+        public static Func<T1, R> ApplyR<T1, T2, R>(this Func<T1, T2, R> func, T2 t2)
+            => t1 => func(t1, t2);
     }
 }
