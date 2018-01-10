8f5bf5d17efa4d670e5d0f7b203a0f06f1c40641
Patrick Drechsler <patrick.drechsler@redheads.de>
2018-01-10 15:25:25 +0100

    IEnumerable.Map in terms of Aggregate

--- chapter07/src/Exercise5.cs
+++ chapter07/src/Exercise5.cs
@@ -2,8 +2,10 @@ using LaYumba.Functional;
 using static LaYumba.Functional.F;
 
 using System;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Chapter07
 {
@@ -12,5 +14,31 @@ namespace Chapter07
      */
     public class Exercise5
     {
+        [Fact]
+        public void Map_with_Aggregate_works() 
+            => Range(1, 5).Map(x => x * x).Should().Equal(1, 4, 9, 16, 25);
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<R> Map<T, R>(this IEnumerable<T> list, Func<T, R> func) 
+            => list.Aggregate(new List<R>(), (acc, t) 
+                => 
+                {
+                    acc.Add(func(t)); /* NOTE: .Add(...) returns void... */
+                    return acc; /* ... that is why we have to return the list "acc" */
+                });
+
+
+        // public static IEnumerable<R> Where<T, R>(this IEnumerable<T> @this, Func<T, R> func)
+        // {
+        //     return null;
+        // }
+
+        // public static IEnumerable<R> Bind<T, R>(this IEnumerable<T> @this, Func<T, R> func)
+        // {
+        //     return null;
+        // }
+
     }
 }
